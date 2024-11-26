e53016a4cbcc34cc4c53d2eeee40382a7767b331
dam824 <damiendagory@gmail.com>
2024-11-26 21:21:33 +0100

    update buffering error to account

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,28 +1,48 @@
-
+import UsernameForm from "@/components/forms/UsernameForm";
+import { Page } from "@/models/Page";
+import mongoose from "mongoose"; // Ajout direct de Mongoose pour test
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "../api/auth/[...nextauth]/route";
-import UsernameForm from "@/components/forms/UsernameForm";
-import { Page } from "@/models/Page";
+
 export default async function AccountPage({ searchParams }) {
+  if (mongoose.connection.readyState === 0) {
+    try {
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 10000,
+      });
+    } catch (error) {
+       
+      throw new Error("Connexion Mongoose impossible");
+    }
+  }
 
   const session = await getServerSession(authOptions);
-  const desiredUsername = searchParams?.desiredUsername;
-  
   if (!session) {
-    return redirect('/');
+    return redirect("/");
   }
-  const page = await Page.findOne({owner: session?.user?.email})
-  
-  if(page){
-   
-    return(
-      <div>Votre slug est : /{page.uri} & votre email est : {page.owner} </div>
-    )
+
+  const desiredUsername = searchParams?.desiredUsername;
+
+  try {
+    const page = await Page.findOne({ owner: session?.user?.email });
+
+    if (page) {
+      return (
+        <div>
+          Votre slug est : /{page.uri} & votre email est : {page.owner}
+        </div>
+      );
+    }
+  } catch (error) {
+    throw new Error("Erreur dans Page.findOne");
   }
+
   return (
     <div>
-   <UsernameForm desireUsername = {desiredUsername} />
+      <UsernameForm desireUsername={desiredUsername} />
     </div>
   );
 }
